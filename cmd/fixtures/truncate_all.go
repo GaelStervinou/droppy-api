bf4b04577c684d2db11a33d3d09bdfca6d633ec1
Dregert <rui.soares282@gmail.com>
2024-07-16 15:16:05 +0200

    hop i manquait ça

--- cmd/fixtures/truncate_all.go
+++ cmd/fixtures/truncate_all.go
@@ -10,5 +10,6 @@ func TruncateTables(db *gorm.DB) {
 	db.Exec("TRUNCATE TABLE comment_responses CASCADE;")
 	db.Exec("TRUNCATE TABLE comments CASCADE;")
 	db.Exec("TRUNCATE TABLE drops CASCADE;")
+	db.Exec("TRUNCATE TABLE drop_notifications CASCADE;")
 	db.Exec("TRUNCATE TABLE users CASCADE;")
 }
