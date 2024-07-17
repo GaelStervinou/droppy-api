fd75bebe290cc173430c4123275565c600dbf94b
Gaël Stervinou <stervinou.g36@gmail.com>
2024-07-17 10:15:52 +0200

    fixes

--- internal/http/middlewares/cors.go
+++ internal/http/middlewares/cors.go
@@ -10,7 +10,7 @@ func Cors() gin.HandlerFunc {
 	fmt.Println("CORS")
 
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
