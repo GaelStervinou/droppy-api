b22e496af09950861c60174594ab3f036e7d115d
Gaël Stervinou <stervinou.g36@gmail.com>
2024-07-17 14:49:18 +0200

    fix group drops fixtures

--- cmd/fixtures/populate_groups.go
+++ cmd/fixtures/populate_groups.go
@@ -14,6 +14,8 @@ func PopulateGroups(db *gorm.DB) {
 
 	images := []string{"https://www.booska-p.com/wp-content/uploads/2022/02/des-mangas-pirate%CC%81s-par-une-firme-americaine-news-visu-1024x750.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTM1eLN0XU2iTWwxwj1HzO5crOgBeZiTA4C7Q&s", "https://lempreintedigitale.com/wp-content/uploads/2022/03/clubs-foot-europeens-plus-suivis-reseaux-sociaux-min.jpeg", "https://f.hellowork.com/edito/sites/3/2021/10/AdobeStock_419775027-2-1-1200x800.jpeg"}
 	memberRoles := postgres.GroupMemberRoles()
+	var lastDropNotification postgres.DropNotification
+	db.Model(&postgres.DropNotification{}).Order("id DESC").Limit(1).Select("id").First(&lastDropNotification)
 	for range 500 {
 		grp := postgres.Group{
 			Name:        faker.Word(),
@@ -29,23 +31,24 @@ func PopulateGroups(db *gorm.DB) {
 			activeUsers[i], activeUsers[j] = activeUsers[j], activeUsers[i]
 		}
 		randNbUsers := rand.Intn(100)
+		var userIdsInGroup []uint
 		for j := range randNbUsers {
 			activeUser := activeUsers[j]
+			userIdsInGroup = append(userIdsInGroup, activeUser.ID)
 			db.Create(&postgres.GroupMember{
 				GroupID:  grp.ID,
 				MemberID: activeUser.ID,
 				Status:   1,
 				Role:     memberRoles[rand.Intn(len(memberRoles))],
 			})
-
-			var dropIds []uint
-			db.Model(&postgres.Drop{}).Where("created_by_id = ?", activeUser.ID).Limit(rand.Intn(5)).Pluck("id", &dropIds)
-			for _, dropId := range dropIds {
-				db.Create(&postgres.GroupDrop{
-					GroupID: grp.ID,
-					DropID:  dropId,
-				})
-			}
+		}
+		var dropIds []uint
+		db.Model(&postgres.Drop{}).Where("created_by_id IN(?) AND drop_notification_id = ?", userIdsInGroup, lastDropNotification.ID).Limit(rand.Intn(30)).Pluck("id", &dropIds)
+		for _, dropId := range dropIds {
+			db.Create(&postgres.GroupDrop{
+				GroupID: grp.ID,
+				DropID:  dropId,
+			})
 		}
 	}
 }
