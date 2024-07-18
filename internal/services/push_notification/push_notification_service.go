890e159b14e3dcd48708921337192fff4cb07d3e
LouisAntoine <louisantoine.pro@gmail.com>
2024-07-18 14:52:09 +0200

    Upgrade push notif service

--- internal/services/push_notification/push_notification_service.go
+++ internal/services/push_notification/push_notification_service.go
@@ -16,14 +16,11 @@ type PushNotificationService struct {
 	Repo *repositories.Repositories
 }
 
-func (s *PushNotificationService) SendNotification(fcmTokens []string, dropType string) {
+func (s *PushNotificationService) SendDropNotification(fcmTokens []string, dropType string) {
 	if len(fcmTokens) == 0 {
 		log.Println("No FCM tokens to send notifications to")
 		return
 	}
-
-	fmt.Println(len(fcmTokens), "FCM tokens found")
-	fmt.Println(fcmTokens[0])
 
 	firebaseRepo, err := firebase.NewRepo()
 	if err != nil {
@@ -91,7 +88,7 @@ func (s *PushNotificationService) SendNotificationsToAllUser(dropType string) {
 		}
 	}
 
-	s.SendNotification(validTokens, dropType)
+	s.SendDropNotification(validTokens, dropType)
 }
 
 func (s *PushNotificationService) GenerateRandomNotification(dropType string) {
@@ -116,4 +113,82 @@ func (s *PushNotificationService) GenerateRandomNotification(dropType string) {
 	time.Sleep(duration)
 
 	s.SendNotificationsToAllUser(dropType)
+}
+
+func (s *PushNotificationService) SendNotification(notifType string, fcmTokens []string) error {
+	if len(fcmTokens) == 0 {
+		log.Println("No FCM tokens to send notifications to")
+		return nil
+	}
+
+	firebaseRepo, err := firebase.NewRepo()
+	if err != nil {
+		log.Println("Error getting firebase repo:", err)
+		return err
+	}
+
+	client, err := firebaseRepo.App.Messaging(context.Background())
+	if err != nil {
+		log.Println("Error getting messaging client:", err)
+		return err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if len(fcmTokens) == 1 {
+		response2, err2 := client.Send(ctx, &messaging.Message{
+			Notification: GetNotificationContent(notifType),
+			Token:        fcmTokens[0],
+		})
+
+		if err2 != nil {
+			log.Println("Error sending single message:", err2)
+		} else {
+			fmt.Println("Single message response:", response2)
+		}
+	} else {
+		response, err := client.SendEachForMulticast(ctx, &messaging.MulticastMessage{
+			Notification: GetNotificationContent(notifType),
+			Tokens:       fcmTokens,
+		})
+
+		if err != nil {
+			log.Println("Error sending multicast message:", err)
+		} else {
+			fmt.Println("Multicast response:", response)
+		}
+	}
+
+	return nil
+}
+
+func GetNotificationContent(notifType string) *messaging.Notification {
+	switch notifType {
+	case "follow-public":
+		return &messaging.Notification{
+			Title: "Nouveau follower !",
+			Body:  "Vous avez un nouvel ami",
+		}
+	case "follow-private":
+		return &messaging.Notification{
+			Title: "Demande d'ami",
+			Body:  "Vous avez une nouvelle demande d'ami",
+		}
+	case "like":
+		return &messaging.Notification{
+			Title: "Nouveau like !",
+			Body:  "Quelqu'un a aimé votre contenu",
+		}
+	case "comment":
+		return &messaging.Notification{
+			Title: "Nouveau commentaire !",
+			Body:  "Quelqu'un a commenté votre contenu",
+		}
+	default:
+		return &messaging.Notification{
+			Title: "Nouveau post",
+			Body:  "Un de vos amis a posté un contenu, allez voir !",
+		}
+	}
 }
